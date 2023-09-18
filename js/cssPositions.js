8f6af9005287744122fdc7516b44cefa4f31e72e
Rolf Häckel <rolf.haeckel@mac.com>
2023-09-18 12:27:25 +0200

    alle Eingabe und Ausgabefelder mit css-Variablen positioniert

--- js/cssPositions.js
+++ js/cssPositions.js
@@ -1,4 +1,4 @@
-function placeDatum() {
-    let datum0 = document.querySelector('datum0');
-    datum0.style.setProperty('left' datumLeft)
-}
+// function placeDatum() {
+//     let datum0 = document.querySelector('datum0');
+//     datum0.style.setProperty('left' datumLeft)
+// }
