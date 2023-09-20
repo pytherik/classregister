3ae559c03557ee58b80b365055a622b813c63355
Maiki <maikpeters1511@gmail.com>
2023-09-20 10:50:29 +0200

    "Adjust font settings, optimize checkbox and input size"
    The font-family setting previously applied to all labels has now been moved specifically to each checkbox class in 'oneweek.css', for improved layout consistency. Also, the font-size of the checkbox labels has been increased for better readability. The left position of the checkboxes has been decreased to ensure appropriate alignment. In the 'oneweek.php' files, the width of text input fields has been reduced to provide improved layout on narrower screens. The starting top position in the 'Module' section and its dynamic increments have been fine-tuned for a more compact UI.

--- view/oneweek.php
+++ view/oneweek.php
@@ -33,47 +33,50 @@
         <div id="notice"><input type="text" name="notice" style="width: 1500px;" value="<?php
             echo $notice; ?>"></div>
         <!-- einträge -->
-        <div id="lesson0"><input name="lesson[]" type="text" style="width: 1200px;"
+        <div id="lesson0"><input name="lesson[]" type="text" style="width: 1100px;"
                                   value="<?php
                                   echo $lesson[0]->getAmContent(); ?>"></div>
-        <div id="lesson1"><input name="lesson[]" type="text" style="width: 1200px;"
+        <div id="lesson1"><input name="lesson[]" type="text" style="width: 1100px;"
                                   value="<?php
                                   echo $lesson[0]->getPmContent(); ?>"></div>
-        <div id="lesson2"><input name="lesson[]" style="width: 1200px;" type="text"
+        <div id="lesson2"><input name="lesson[]" style="width: 1100px;" type="text"
                                   value="<?php
                                   echo $lesson[1]->getAmContent(); ?>"></div>
-        <div id="lesson3"><input name="lesson[]" type="text" style="width: 1200px;"
+        <div id="lesson3"><input name="lesson[]" type="text" style="width: 1100px;"
                                   value="<?php
                                   echo $lesson[1]->getPmContent(); ?>"></div>
-        <div id="lesson4"><input name="lesson[]" type="text" style="width: 1200px;"
+        <div id="lesson4"><input name="lesson[]" type="text" style="width: 1100px;"
                                   value="<?php
                                   echo $lesson[2]->getAmContent(); ?>"></div>
-        <div id="lesson5"><input name="lesson[]" type="text" style="width: 1200px;"
+        <div id="lesson5"><input name="lesson[]" type="text" style="width: 1100px;"
                                   value="<?php
                                   echo $lesson[2]->getPmContent(); ?>"></div>
-        <div id="lesson6"><input name="lesson[]" type="text" style="width: 1200px;"
+        <div id="lesson6"><input name="lesson[]" type="text" style="width: 1100px;"
                                   value="<?php
                                   echo $lesson[3]->getAmContent(); ?>"></div>
-        <div id="lesson7"><input name="lesson[]" type="text" style="width: 1200px;"
+        <div id="lesson7"><input name="lesson[]" type="text" style="width: 1100px;"
                                   value="<?php
                                   echo $lesson[3]->getPmContent(); ?>"></div>
-        <div id="lesson8"><input name="lesson[]" type="text" style="width: 1200px;"
+        <div id="lesson8"><input name="lesson[]" type="text" style="width: 1100px;"
                                   value="<?php
                                   echo $lesson[4]->getAmContent(); ?>"></div>
-        <div id="lesson9"><input name="lesson[]" type="text" style="width: 1200px;"
+        <div id="lesson9"><input name="lesson[]" type="text" style="width: 1100px;"
                                   value="<?php
                                   echo $lesson[4]->getPmContent(); ?>"></div>
 
         <!-- Module -->
                 <?php
-                $top_position = 560; // Startposition
+                $top_position = 550; // Startposition
                 for ($i = 0; $i < 5; $i++) {
                     foreach ($options as $index => $option) {
                         echo "<div id=\"FormOfInstruction" . ($index + 1) . "\" class=\"checkbox" . ($index + 1) . "\" style=\"top: " . $top_position . "px;\">";
                         echo "<input type=\"checkbox\" name=\"checkbox[]\" value=\"" . $option . "\">";
                         echo "<label for=\"FormOfInstruction\">" . $option . "</label>";
                         echo "</div>";
-                        $top_position += 25;
+                        $top_position += 20;
+                        if ($index == 1) {
+                            $top_position += 21;
+                        }
                     }
                     $top_position += 57;
                 }
